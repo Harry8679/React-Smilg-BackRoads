c3bbc0a3bd9dd35da616440a290dedbab989d4d0
Harry EMARH <emarh.harry.code@gmail.com>
2024-07-04 23:20:27 +0200

    Hero Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <section className="hero" id="home">
-        <div className="hero-banner">
-          <h1>continue exploring</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
-            explicabo debitis est autem dicta.
-          </p>
-          <a href="#tours" className="btn hero-btn">
-            explore tours
-          </a>
-        </div>
-      </section>
+      <Hero />
 
       <section className="section" id="about">
         <div className="section-title">
