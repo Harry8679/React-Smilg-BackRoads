4b2be07ac6108867a8170dec1fe8d0b500afc3d2
Harry EMARH <emarh.harry.code@gmail.com>
2024-07-10 14:16:25 +0200

    Refactoring of Services Component

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,19 +7,20 @@ const Services = () => {
     <section className="section services" id="services">
       <Title title="our" subTitle="services" />
       <div className="section-center services-center">
-        {services.map((service) => (
-          <article className="service" key={service.id}>
-            <span className="service-icon">
-              <i className={service.icon}></i>
-            </span>
-            <div className="service-info">
-              <h4 className="service-title">{service.title}</h4>
-              <p className="service-text">
-                {service.text}
-              </p>
-            </div>
-          </article>
-        ))}
+        {services.map((service) => {
+          const { id, icon, title, text } = service;
+          return (
+            <article className="service" key={id}>
+              <span className="service-icon">
+                <i className={icon}></i>
+              </span>
+              <div className="service-info">
+                <h4 className="service-title">{title}</h4>
+                <p className="service-text">{text}</p>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
